c7c1babf9522172c5753312c3af9dd14f16fb607
Evyatarr <evyata221@gmail.com>
2024-02-06 14:45:42 +0200

    added Bootstrap and SASS

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
 
   return (
     <>
+      {/* <>NavBar</> */}
       <img src={hadarPic} className="logo" alt="Hadar"></img>
       <h1>לזכרו של הדר קפלוק ז״ל</h1>
       <p>..האתר בהקמה </p>
       <div className="card">
+        <button class="x">Primary button</button>
         <button onClick={() => setHeart((heart) => heart + "❤️")}>
           {heart}
         </button>
