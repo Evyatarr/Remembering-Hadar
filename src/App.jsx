9e8c103e1d3e2a1edbf6584acc5c2f16abbabd68
Evyatarr <evyata221@gmail.com>
2024-02-06 14:57:30 +0200

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Navbar from "./Navbar.jsx";
 import "./App.css";
 import hadarPic from "./assets/hadar001.jpg";
 
@@ -7,12 +8,11 @@ function App() {
 
   return (
     <>
-      {/* <>NavBar</> */}
+      <Navbar />
       <img src={hadarPic} className="logo" alt="Hadar"></img>
       <h1>לזכרו של הדר קפלוק ז״ל</h1>
       <p>..האתר בהקמה </p>
       <div className="card">
-        <button class="x">Primary button</button>
         <button onClick={() => setHeart((heart) => heart + "❤️")}>
           {heart}
         </button>
